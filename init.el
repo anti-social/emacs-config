ae57689e1f8d9464a4bfda12e4a8d0b9a79276f0
Alexander Koval <kovalidis@gmail.com>
2016-10-05 20:28:02 +0300

    Basic settings

---
+++ init.el
@@ -0,0 +1,22 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+(require 'init-elpa)
+
+(require 'init-ui)
+
+(require 'init-editing)
+
+(require 'init-company-mode)
+
+(require 'init-python)
+
+(require 'init-rust)
+
+(provide 'init)
---
+++ lisp/init-rust.el
@@ -0,0 +1,34 @@
+(require 'init-elpa)
+(require-package 'company)
+(require-package 'racer)
+(require-package 'rust-mode)
+(require-package 'flycheck)
+(require-package 'flycheck-rust)
+
+(require 'company)
+(require 'racer)
+(require 'rust-mode)
+(require 'electric)
+(require 'eldoc)
+(require 'flycheck)
+(require 'flycheck-rust)
+
+(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+(add-hook 'rust-mode-hook #'company-mode)
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+(add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+(add-hook 'rust-mode-hook
+          '(lambda ()
+	     ;; (setq racer-cmd (concat (getenv "HOME") "/.rust-dev/racer/target/release/racer"))
+	     ;; (setq racer-rust-src-path (concat (getenv "HOME") "/.rust-dev/rust/src"))
+	     (setq racer-cmd "/usr/local/bin/racer")
+	     (setq racer-rust-src-path "/usr/src/rust/src")
+             (local-set-key (kbd "TAB") #'company-indent-or-complete-common)
+	     (electric-pair-mode 1)))
+
+;; (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+
+(add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+
+(provide 'init-rust)
