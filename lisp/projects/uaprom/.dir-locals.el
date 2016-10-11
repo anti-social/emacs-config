c55eed9d79a324ec7c059568921c83a00384fbcb
Alexander Koval <kovalidis@gmail.com>
2016-10-11 19:09:22 +0300

    Added directory variables for uaprom project

---
+++ lisp/projects/uaprom/.dir-locals.el
@@ -0,0 +1,5 @@
+((python-mode . ((python-shell-interpreter . "/home/alexk/projects/uaprom/.vagga/uaprom/usr/bin/python2.7")
+                 (python-check-command . "pyflakes-python2")
+                 (eval . (setenv "PYTHONPATH"
+                                 (concat "/home/alexk/projects/uaprom/.vagga/uaprom/usr/local/lib/python2.7/dist-packages" ":"
+                                         "/home/alexk/projects/uaprom/.vagga/uaprom/usr/lib/python2.7"))))))
